84498e516ce4ac0bad959fbb7654afd1df6935c7
Abhishek Kumar Singh <ram07abhishek@gmail.com>
2026-03-13 14:20:11 +0530

    new commit

--- 8.promises/index.js
+++ 8.promises/index.js
@@ -1,3 +1,29 @@
-function delay(time){
-     
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time);
+  });
 }
+
+console.log("promise Lecture Starts");
+delay(2000).then(() => {
+  console.log("After 2 seconds Promise Resolved");
+});
+console.log("End");
+
+function myDivide(num1, num2) {
+  return new Promise((res, rej) => {
+    if (num2 === 0) {
+      rej("Can not divide it br,sorry");
+    } else {
+      res(num1 / num2);
+    }
+  });
+}
+
+myDivide(10, 5)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
+
+  myDivide(10, 5)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
